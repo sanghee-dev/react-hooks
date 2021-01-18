d9204978f0327555c122d75e9b9175b1272d4d11
leeedev <leeesangheee@gmail.com>
2021-01-18 22:59:23 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div className="container"></div>;
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Hello, useState!</h1>
+      <h4>현재 숫자는 {number}</h4>
+      <button onClick={() => setNumber(number + 1)}>+</button>
+      <button onClick={() => setNumber(number - 1)}>-</button>
+    </div>
+  );
 };
 
 export default App;
