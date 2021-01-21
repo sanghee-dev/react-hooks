eb6e3bc6d8889ffe5002c6ca70c646c8af758569
leeedev <leeesangheee@gmail.com>
2021-01-22 02:30:12 +0900

    useFullscreen

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef } from "react";
 
-const useScroll = () => {
-  const [state, setState] = useState({ x: 0, y: 0 });
-  const onScroll = () => {
-    setState({ x: window.scrollX, y: window.scrollY });
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
   };
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, []);
-  const style = {
-    position: "fixed",
-    color: `rgba(${255 - 255 * (state.y / window.innerHeight / 5)}, ${
-      255 * (state.y / window.innerHeight / 5)
-    }, ${255 * (state.y / window.innerHeight / 5)}, 1)`,
+  const triggerFull = () => {
+    if (element.current.requestFullscreen) {
+      element.current.requestFullscreen();
+    } else if (element.current.mozRequestFullScreen) {
+      element.current.mozRequestFullScreen();
+    } else if (element.current.webkitRequestFullscreen) {
+      element.current.webkitRequestFullscreen();
+    } else if (element.current.msRequestFullscreen) {
+      element.current.msRequestFullscreen();
+    }
+    runCb(true);
   };
-  return { ...state, style };
+  const exitFull = () => {
+    document.exitFullscreen();
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
 };
 
 const App = () => {
-  const { x, y, style } = useScroll();
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
   return (
-    <div className="container" style={{ height: "500vh" }}>
-      <h1 style={style}>Hello, useScroll!</h1>
+    <div className="container">
+      <div
+        ref={element}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1>Hello, useFullscreen!</h1>
+        <img
+          src="https://item.kakaocdn.net/do/7f552a5d038cc7b0b6f99339bbc330baf43ad912ad8dd55b04db6a64cddaf76d"
+          style={{ width: window.innerHeight / 2 }}
+          alt="React"
+        />
+        <button onClick={triggerFull}>Make Fullscreen</button>
+        <button onClick={exitFull}>Exit Fullscreen</button>
+      </div>
     </div>
   );
 };
