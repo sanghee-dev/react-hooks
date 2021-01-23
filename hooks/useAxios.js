1e71e9eab6f8e046bc8355d431461358ceab286d
leeedev <leeesangheee@gmail.com>
2021-01-23 19:17:06 +0900

    useAxios

--- hooks/useAxios.js
+++ hooks/useAxios.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from "react";
-import defaultAxios from "axios";
-
-const useAxios = (opts, axiosInstance = defaultAxios) => {
+export const useAxios = (opts, axiosInstance = defaultAxios) => {
   const [state, setState] = useState({
     loading: true,
     data: null,
@@ -34,20 +31,3 @@ const useAxios = (opts, axiosInstance = defaultAxios) => {
   }
   return { ...state, refetch };
 };
-
-const App = () => {
-  const { loading, data, error, refetch } = useAxios({
-    url:
-      "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json",
-  });
-  return (
-    <div className="container">
-      <h1>Hello, useAxios!</h1>
-      <h4>{data && data.status}</h4>
-      <h4>{loading && "Loading"}</h4>
-      <button onClick={refetch}>Click me!</button>
-    </div>
-  );
-};
-
-export default App;
